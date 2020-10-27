a5c5fcf3ea24475b90fb9efe423d03a0e7dcf5be
Cameron Bhatnagar <cbhatnag@uncc.edu>
2020-10-27 14:28:21 -0400

    Notes app initial commit

---
+++ notes.py
@@ -0,0 +1,19 @@
+# imports
+import os
+from flask import Flask
+
+app = Flask(__name__)     # create an app
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+
+# To see the web page in your web browser, go to the url,
+#   http://127.0.0.1:5000
+
+# Note that we are running with "debug=True", so if you make changes and save it
+# the server will automatically update. This is great for development but is a
+# security risk for production.
